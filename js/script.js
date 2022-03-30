a296ffa7f29c3496e7c149bf2227cf055a7e1021
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-03-30 17:43:16 -0400

    fixed employeesCard fucntioin and commented out code in index.html

--- js/script.js
+++ js/script.js
@@ -5,48 +5,50 @@ const image = document.querySelector('.profile');
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");;
 let employees = [];
-let browserHTML;
+let browserHTML = '';
 let modalHTML;
 
 
 // Fetch Function //
-async function fetchData(url) {           // look into making this into an async function //
-    try {
-        const res = await fetch(url);
-        const res_1 = await res.json();
-        const data = res_1.results;
-        return employeeCards(data);
-    } catch (err) {
-        return console.log(err);
-    }
+function fetchData(url) {           // look into making this into an async function //
+    return fetch(url)
+            .then(res => res.json())
+            .then(data => employeeCards(data.results))
+            .catch(err => console.log(err))
+
 }
 
 console.log(fetchData('https://randomuser.me/api/?results=12&inc=name,email,location,picture,cell,dob'))
 
 
+// fetch('https://randomuser.me/api/?results=12&nat=us&inc=name,email,location,picture,cell,dob')
+// .then(res => res.json())
+// .then(data => employeeCards(data.results))
+// .catch(err => console.log(err))
+
 // Helper Functions //
 const employeeCards = (data) => {
     employees = data;
-    let name = data.name
-    let email = data.email;
-    let city = data.location.city;
-    let state = data.location.state;
-    let picture = data.picture;
-    
-    for(let i = 0; i < 12;i++) {    // instead of putting length 12,  can i say ex. i < array.length, or something else
-    browserHTML = `
-    <div class="card" data-index="${i}">
-        <img class="profile" src="${picture.thumbnail}">
-        <div class="text-container">
-            <h2 class="name">${name.first} ${name.last}</h2>
-                <p class="contact">${email}</p>
-                <p class="contact">${city}, ${state}</p>
-        </div>
-    </div>
-    `;
-    console.log(browserHTML)
-    };
+    employees.map((employee, i) => {
+        let name = employee.name
+        let email = employee.email;
+        let city = employee.location.city;
+        let state = employee.location.state;
+        let picture = employee.picture;
+        browserHTML += `
+            <div class="card" data-index="${i}">
+                <img class="profile" src="${picture.thumbnail}">
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                        <p class="contact">${email}</p>
+                        <p class="contact">${city}, ${state}</p>
+                </div>
+            </div> 
+            `;
+    });
+    container.innerHTML = browserHTML;
 }
+
 
 
 // Modal Window Functions //
