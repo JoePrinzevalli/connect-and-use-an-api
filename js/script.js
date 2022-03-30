e2759a578b26354352ba2284f33e852fa9dd566a
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-03-30 00:38:24 -0400

    added variables and started fetch fcuntion in js

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Variables //
+const container = document.querySelector('.container');
+const overlay = document.querySelector('.overlay')
+const image = document.querySelector('.profile');
+const modalContainer = document.querySelector(".modal-content");
+const modalClose = document.querySelector(".modal-close");;
+let employees = [];
+
+// Fetch Function //
+
+function fetchData(url) {           // look into making this into an async function //
+    return fetch(url)
+        .then(res => res.json())
+        .then(res => res.results)
+        // .then(employeeCards)
+        .catch(err => console.log(err))
+}
+console.log(fetchData('https://randomuser.me/api/'))
+
+
+// HTML Maker //
+
+
+
+// Modal Window Functions //
+
+
+
